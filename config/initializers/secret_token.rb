e1b934d5483696249c1ea2bde30f085de76d4ad0
waqas1991 <waqas.ahmed@tenpearls.com>
2013-11-18 10:17:05 +0500

    Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitHub::Application.config.secret_key_base = '3298d26738c2998ee050a6710283f027c94ab96e4ba91f35b0b0f9c755e5709a4a9cbcb4c164fef7db478f417f74fa99997879e909b079fc1181f2c05c43c939'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GitHub::Application.config.session_store :cookie_store, key: '_GitHub_session'
